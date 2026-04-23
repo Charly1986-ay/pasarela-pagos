69c0451527adb37ed3e8a2d6a41a846bc8301389
Charly Ay <Charly1986-ay@users.noreply.github.com>
2026-04-23 19:13:24 -0300

    mi primer pago con stripe

---
+++ stripe_payments_gateway.py
@@ -0,0 +1,48 @@
+import os
+import dotenv
+import stripe
+
+# Configuración del entorno
+dotenv.load_dotenv()
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+
+stripe.api_key=STRIPE_SECRET_KEY
+
+# Crear un metodo de pago
+def create_payment_method() -> str:
+
+    try:
+        # Aca se genera el metodo de pago
+        payment_method = stripe.PaymentMethod.create(
+            type='card',
+            card={'token': 'tok_visa'}
+        )
+        print(payment_method)
+
+        print(f'Método de pago con ID: {payment_method.id}')
+
+        return payment_method.id
+    except stripe.StripeError as e:
+        print(e.user_message)
+
+
+# crear un pago
+def create_payment(payment_method_id):
+    try:
+        payment=stripe.PaymentIntent.create(
+            # OJO sea usd o euros hay que convertirlos en CENTIMOS
+            amount=5 * 100, # se multiplica por 100
+            currency='usd',
+            payment_method=payment_method_id,
+            payment_method_types=['card'],
+            confirm=True
+        )
+
+        print(f'Pago con ID {payment.id} realizada correctamente.')
+    except stripe.CardError as e:
+        print(f'Error en la tarjeta: {e.user_message}')
+    except stripe.StripeError as e:
+        print(f'Error en stripe: {e.user_message}')
+
+payment_method_id=create_payment_method()
+create_payment(payment_method_id=payment_method_id)
