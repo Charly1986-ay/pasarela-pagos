fa5b9e4df3582eaba70208b280a7d4c9ef6ece08
Charly Ay <Charly1986-ay@users.noreply.github.com>
2026-04-23 22:18:20 -0300

    prueba final

--- stripe_payments_gateway.py
+++ stripe_payments_gateway.py
@@ -2,86 +2,120 @@ import os
 import dotenv
 import stripe
 
+
 # Configuración del entorno
+
 dotenv.load_dotenv()
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
-stripe.api_key=STRIPE_SECRET_KEY
+stripe.api_key = STRIPE_SECRET_KEY
 
-# Crear un metodo de pago
+# Crear método de pago
+
+
 def create_payment_method() -> str:
 
     try:
-        # Aca se genera el metodo de pago
         payment_method = stripe.PaymentMethod.create(
-            type='card',
-            card={'token': 'tok_visa'}
+            type="card",
+            card={"token": "tok_visa"}
         )
-        print(payment_method)
 
-        print(f'Método de pago con ID: {payment_method.id}')
+        print(f"Método de pago creado con ID: {payment_method.id}")
 
         return payment_method.id
     except stripe.StripeError as e:
-        print(e.user_message)
+        print(f"Error en Stripe: {e.user_message}")
+
+# Crear un pago
 
 
-# crear un pago
-def create_payment(client_id: str, payment_method_id: str):
+def create_payment(client_id: str, payment_method_id: str, product_id: str, amount: int, currency: str):
+
     try:
-        payment=stripe.PaymentIntent.create(
-            # OJO sea usd o euros hay que convertirlos en CENTIMOS
-            amount=5 * 100, # se multiplica por 100
-            currency='usd',
+
+        payment = stripe.PaymentIntent.create(
+            amount=amount,
+            currency=currency,
             customer=client_id,
             payment_method=payment_method_id,
-            payment_method_types=['card'],
-            confirm=True
+            payment_method_types=["card"],
+            confirm=True,
+            metadata={
+                "product_id": product_id
+            }
         )
 
-        print(f'Pago con ID {payment.id} realizada correctamente.')
+        print(f"Pago con ID {payment.id} realizado correctamente.")
+
     except stripe.CardError as e:
-        print(f'Error en la tarjeta: {e.user_message}')
+        print(f"Error en la tarjeta: {e.user_message}")
     except stripe.StripeError as e:
-        print(f'Error en stripe: {e.user_message}')
+        print(f"Error en Stripe: {e.user_message}")
+
+# Crear usuario
 
 
-# creacion de usuario segun al api stripe
-def create_user(name: str, email:str):
+def create_user(name, email):
+
     try:
         client = stripe.Customer.create(
             name=name,
             email=email
         )
-        print(f'Cliente {client.name} creado correctamente con ID: {client.id}')
+
+        print(f"Client {name} creado correctamente con ID {client.id}")
 
         return client.id
     except stripe.StripeError as e:
-        print(f'Error en stripe: {e.user_message}')
+        print(f"Error en Stripe: {e.user_message}")
+
+# Asociar método de pago a usuario
 
 
-# Asociar el método de pago al usuario
 def add_payment_method_to_user(client_id, payment_method_id):
-    # asocia el cliente con el método de pago
-    stripe.PaymentMethod.attach(
-        payment_method_id=payment_method_id,
-        customer=client_id
-    )
 
-    print('método de pago asociado al usuario')
+    try:
+        stripe.PaymentMethod.attach(
+            payment_method_id,
+            customer=client_id
+        )
 
+        print("Método de pago asociado al usuario.")
+
+    except stripe.StripeError as e:
+        print(f"Error en Stripe: {e.user_message}")
+
+# Obtener productos
+
+
+def get_product():
+
+    products = stripe.Product.list(limit=1)
+
+    return products["data"][0]["id"]
+
+
+def get_product_price(product_id):
+
+    price = stripe.Price.list(product=product_id, limit=1)
+
+    price_id = price["data"][0]["id"]
+    amount = price["data"][0]["unit_amount"]
+    currency = price["data"][0]["currency"]
+
+    return price_id, amount, currency
 
 # Prueba
-client_id = create_user(name='Pepito Perez', email='pepito@example.com')
 
-payment_method_id=create_payment_method()
 
-add_payment_method_to_user(
-    client_id=client_id, 
-    payment_method_id=payment_method_id
-)
+client_id = create_user("jaimito", "jaimito@gmail.com")
 
-create_payment(
-    client_id=client_id, 
-    payment_method_id=payment_method_id
-)
+payment_method_id = create_payment_method()
+
+add_payment_method_to_user(client_id, payment_method_id)
+
+product_id = get_product()
+price_id, amount, currency = get_product_price(product_id)
+
+create_payment(client_id, payment_method_id, product_id, amount, currency)
